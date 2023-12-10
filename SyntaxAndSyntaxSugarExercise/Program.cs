9d4f3dba8169a647cfc9e152e8afd31815440b3c
William Thompson <wmthmps1@gmail.com>
2023-12-09 20:11:02 -0600

    Initial Commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
+            
         }
     }
 }
+/*
+
+
+ Given the following code, change the code to implement:
+inferred typing
+string interpolation
+and the ternary operator
+ 
+ */
